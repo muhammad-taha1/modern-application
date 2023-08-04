c3d20f44e6b661148f1b41a0c2c8bfeda587a044
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2023-08-04 17:09:19 -0400

    created a new service for notifications

---
+++ notification-service/src/main/java/com/modern/app/infrastructure/repository/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains implementation of repositories, as defined in output ports in application layer.
+ * The connectivity with databases and database specific logic should be contained here
+ */
+package com.modern.app.infrastructure.repository;
